a35ec2f24ca47c9e012927673f8bed2a14e79bab
Saurav Shrestha <saurastha@gmail.com>
2024-01-26 22:25:08 +0545

    Add import statement and device agnostic constant

---
+++ app/constants/__init__.py
@@ -0,0 +1,1 @@
+from app.constants.constants import *
--- app/constants/constants.py
+++ app/constants/constants.py
@@ -1,3 +1,11 @@
+import torch
+
+# Model Paths
 SIGN_DETECTOR_MODEL_PATH = "models/SignetDetect.pt"
 SIGN_VERIFIER_MODEL_PATH = "models/SignVerify.pth"
 SIGN_CLEANER_MODEL_PATH = "models/cleaner.pt"
+
+# Device
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+
